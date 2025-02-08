52a5a114185da6b48677e8662d3cbf7bc8e7a1ab
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-08 10:57:37 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'List of objects associated with the tag',
+    '{description}' => 'List of objects associated with the specified tag'
+];
